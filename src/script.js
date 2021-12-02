119fc82252f1899ca0c253b2fa50485c90465895
tracksuite <youngunner1998@gmail.com>
2021-12-02 14:21:21 +0530

    completed lesson 15

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 /**
  * Base
  */
@@ -58,7 +58,30 @@ scene.add(rectAreaLight)
 //Spot light
 const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
 spotLight.position.set(0, 4, 3)
+spotLight.target.position.x = - 0.75
 scene.add(spotLight)
+
+/**
+ * Helpers
+ */
+ const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+ scene.add(rectAreaLightHelper)
+ 
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(directionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
+window.requestAnimationFrame(() =>
+{
+    spotLightHelper.update()
+})
 
 /**
  * Objects
